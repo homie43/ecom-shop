e660b24ae410fb35dca598b8848077589c8fee94
Vladimir <homie43@yandex.ru>
2023-01-11 14:49:35 +0300

    section timer completed

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -5,13 +5,15 @@ import { Container, Row, Col } from "reactstrap";
 
 import Helmet from "../../components/Helmet/Helmet";
 import Services from "../../services/Services";
-
+import ProductsList from "../../UI/ProductsList";
+import Clock from "../../UI/Clock";
 import products from "../../assets/data/products";
+
+import counterImg from "../../assets/images/counter-timer-img.png";
 
 import heroImg from "../../assets/images/hero-img.png";
 
-import "./Home.scss";
-import ProductsList from "../../UI/ProductsList";
+import "./home.scss";
 
 const Home = () => {
   const [trendingProducts, setTrendingProducts] = React.useState([]);
@@ -47,7 +49,7 @@ const Home = () => {
                   Labore reiciendis tempora adipisci praesentium quidem
                   suscipit. Adipisci doloremque deleniti doloribus officia.
                 </p>
-                <motion.button whileTap={{ scale: 1.1 }} className="buy__btn">
+                <motion.button whileHover={{ scale: 1.1 }} className="buy__btn">
                   <Link to="/shop">SHOP NOW</Link>
                 </motion.button>
               </div>
@@ -84,6 +86,31 @@ const Home = () => {
             </Col>
 
             <ProductsList data={bestProducts} />
+          </Row>
+        </Container>
+      </section>
+
+      <section className="timer__count">
+        <Container>
+          <Row>
+            <Col lg="6" md="6">
+              <div className="clock__top-content">
+                <h4 className="text-white fs-6 mb-2">Limited Offer</h4>
+                <h3 className="text-white fs-5 mb-3">Quality Armchair</h3>
+              </div>
+              <Clock />
+
+              <motion.button
+                whileHover={{ scale: 1.1 }}
+                className="buy__btn store__btn"
+              >
+                <Link to="/shop">Visit Store</Link>
+              </motion.button>
+            </Col>
+
+            <Col lg="6" md="6" className="text-end">
+              <img src={counterImg} alt="" />
+            </Col>
           </Row>
         </Container>
       </section>
