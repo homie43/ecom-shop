06bdbe9c38e8239b82b68fd65b7725d74141d910
Vladimir <homie43@yandex.ru>
2023-01-11 14:04:22 +0300

    data upload, render trending products and best sales, dynamic ProductCard and ProductsList

--- src/UI/ProductCard.jsx
+++ src/UI/ProductCard.jsx
@@ -3,29 +3,27 @@ import { Link } from "react-router-dom";
 import { motion } from "framer-motion";
 import { Col } from "reactstrap";
 
-import productImg from "../assets/images/arm-chair-01.jpg";
-
 import "../SCSS/product-card.scss";
 
-const ProductCard = () => {
+const ProductCard = ({ item }) => {
   return (
-    <Col lg="3" md="4">
+    <Col lg="3" md="4" className="mb-2">
       <div className="product__item">
         <div className="product__img">
           <motion.img
             whileHover={{ scale: 0.9 }}
-            src={productImg}
+            src={item.imgUrl}
             alt="Product"
           />
         </div>
         <div className="p-2 product__info">
           <h3 className="product__name">
-            <Link to="/shop/id">Moderm Armchair</Link>
+            <Link to={`/shop/${item.id}`}>{item.productName}</Link>
           </h3>
-          <span>Chair</span>
+          <span>{item.category}</span>
         </div>
         <div className="product__card-bottom d-flex align-items-center justify-content-between">
-          <span className="price">$299</span>
+          <span className="price">${item.price}</span>
           <motion.span whileTap={{ scale: 1.2 }}>
             <i class="ri-add-line"></i>
           </motion.span>
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -6,13 +6,32 @@ import { Container, Row, Col } from "reactstrap";
 import Helmet from "../../components/Helmet/Helmet";
 import Services from "../../services/Services";
 
+import products from "../../assets/data/products";
+
 import heroImg from "../../assets/images/hero-img.png";
 
 import "./Home.scss";
 import ProductsList from "../../UI/ProductsList";
 
 const Home = () => {
+  const [trendingProducts, setTrendingProducts] = React.useState([]);
+  const [bestProducts, setBestProducts] = React.useState([]);
+
   const year = new Date().getFullYear(); // вернет текущий год
+
+  React.useEffect(() => {
+    const filtredTrendingProducts = products.filter(
+      (item) => item.category === "chair"
+    );
+
+    const filtredBestProducts = products.filter(
+      (item) => item.category === "sofa"
+    );
+
+    setTrendingProducts(filtredTrendingProducts);
+    setBestProducts(filtredBestProducts);
+  }, []);
+
   return (
     <Helmet title={"Home"}>
       <section className="hero__section">
@@ -51,7 +70,20 @@ const Home = () => {
             <Col lg="12" className="text-center">
               <h2 className="section__title">Trending Products</h2>
             </Col>
-            <ProductsList />
+
+            <ProductsList data={trendingProducts} />
+          </Row>
+        </Container>
+      </section>
+
+      <section className="best__sales">
+        <Container>
+          <Row>
+            <Col lg="12" className="text-center">
+              <h2 className="section__title">Best Sales</h2>
+            </Col>
+
+            <ProductsList data={bestProducts} />
           </Row>
         </Container>
       </section>
