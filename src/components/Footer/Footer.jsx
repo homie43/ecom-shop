efbea259a3e39c4c1932f9f59182870a35e2311d
Vladimir <homie43@yandex.ru>
2023-01-13 00:08:47 +0300

    component Footer completed

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,112 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { Container, Row, Col, ListGroup, ListGroupItem } from "reactstrap";
+
+import "./footer.scss";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  const year = new Date().getFullYear();
+  return (
+    <div className="footer">
+      <Container>
+        <Row>
+          <Col lg="4">
+            <div className="logo">
+              <div>
+                <h1 className="text-white">Multimarket</h1>
+              </div>
+            </div>
+            <p className="footer-text mt-4">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima et
+              quae saepe quis doloremque eveniet illum aspernatur ex, soluta
+              pariatur.
+            </p>
+          </Col>
+
+          <Col lg="3">
+            <div className="footer__quick-links">
+              <h4 className="quick__links-title text-white">Top Categories</h4>
+              <ListGroup>
+                <ListGroupItem className="ps-0 border-0">
+                  <Link to="#">Mobile Phones</Link>
+                </ListGroupItem>
+
+                <ListGroupItem className="ps-0 border-0">
+                  <Link to="#">Modern Sofa</Link>
+                </ListGroupItem>
+
+                <ListGroupItem className="ps-0 border-0">
+                  <Link to="#">Arm Chair</Link>
+                </ListGroupItem>
+
+                <ListGroupItem className="ps-0 border-0">
+                  <Link to="#">Smart Watches</Link>
+                </ListGroupItem>
+              </ListGroup>
+            </div>
+          </Col>
+
+          <Col lg="2">
+            <div className="footer__quick-links">
+              <h4 className="quick__links-title text-white">Useful Links</h4>
+              <ListGroup>
+                <ListGroupItem className="ps-0 border-0">
+                  <Link to="/shop">Shop</Link>
+                </ListGroupItem>
+
+                <ListGroupItem className="ps-0 border-0">
+                  <Link to="/cart">Cart</Link>
+                </ListGroupItem>
+
+                <ListGroupItem className="ps-0 border-0">
+                  <Link to="/login">Login</Link>
+                </ListGroupItem>
+
+                <ListGroupItem className="ps-0 border-0">
+                  <Link to="#">Privacy Policy</Link>
+                </ListGroupItem>
+              </ListGroup>
+            </div>
+          </Col>
+
+          <Col lg="3">
+            <div className="footer__quick-links">
+              <h4 className="quick__links-title text-white">Contacts</h4>
+              <ListGroup className="footer__contacts">
+                <ListGroupItem className="ps-0 border-0 d-flex align-items-center gap-2">
+                  <span>
+                    <i class="ri-map-pin-line"></i>
+                  </span>
+                  <p>1, Nevsky Prospect,Saint-Petersburg, Russia</p>
+                </ListGroupItem>
+
+                <ListGroupItem className="ps-0 border-0 d-flex align-items-center gap-2">
+                  <span>
+                    <i class="ri-phone-line"></i>
+                  </span>
+                  <p>+79142010886</p>
+                </ListGroupItem>
+
+                <ListGroupItem className="ps-0 border-0 d-flex align-items-center gap-2">
+                  <span>
+                    <i class="ri-mail-line"></i>
+                  </span>
+                  <p>homie43@yandex.ru</p>
+                </ListGroupItem>
+              </ListGroup>
+            </div>
+          </Col>
+
+          <Col lg="12">
+            <p className="footer__copyright">
+              Copyright {year} developed by{" "}
+              <a href="https://github.com/homie43">Vladimir Kopylov</a>
+            </p>
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
 };
 
 export default Footer;
